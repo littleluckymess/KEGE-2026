551fbc82d4d3aa39b92446664e3623be41b6d67b
User <mila.andreeva2020@mail.ru>
2025-11-08 17:45:06 +0200

    Задание 6

--- task_6/23552.py
+++ task_6/23552.py
@@ -3,3 +3,26 @@ screensize(3000,3000)
 tracer(False)
 m=10
 lt(90)
+for i in range(5):
+    fd(73*m)
+    rt(90)
+    fd(44*m)
+    rt(90)
+up()
+bk(18*m)
+rt(90)
+fd(29*m)
+lt(90)
+down()
+for i in range(5):
+    fd(31*m)
+    rt(90)
+    fd(35*m)
+    rt(90)
+up()
+for x in range(-20,50):
+    for y in range(-2,95):
+        goto(x*m,y*m)
+        dot(3,'red')
+update()
+done()
