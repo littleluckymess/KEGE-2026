322c40d1d34a2561f94357e04bc3e5a54f2dd92d
sunny_milly <mila.andreeva2020@mail.ru>
2025-11-21 19:47:29 +0200

    задание 14

---
+++ task 14/9837.py
@@ -0,0 +1,8 @@
+from string import printable as p
+for x in p[:23]:
+    num1 = int(f'7{x}38596',23)
+    num2 = int(f'14{x}36',23)
+    num3 = int(f'61{x}7',23)
+    num = num1 + num2 + num3
+    if num% 22 == 0:
+        print(x,num//22)
