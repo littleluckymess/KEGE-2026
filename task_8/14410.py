3d9f206e693e260d909e5ecfda2c423349488e17
sunny_milly <mila.andreeva2020@mail.ru>
2025-12-09 18:12:06 +0200

    задание 8

--- task_8/14410.py
+++ task_8/14410.py
@@ -1,6 +1,7 @@
+from itertools import product
 alph = sorted('СОЛНЦЕ')
 cnt = 0
 for pos, val in enumerate(product(alph, repeat = 6), start = 1):
     val = ''.join(val)
-        cnt +=1
+    cnt +=1
 print(cnt)
--- task_8/1936.py
+++
@@ -1,4 +0,0 @@
-cnt = 0
-    val = ''.join(val)
-        cnt += 1
-print(cnt)
