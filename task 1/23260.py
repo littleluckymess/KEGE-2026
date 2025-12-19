1674397c0ff7700ebe4cb53c1768ef887449fa63
User <mila.andreeva2020@mail.ru>
2025-12-19 21:38:39 +0200

    задание 1

---
+++ task 1/23260.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'DC CF FG GE EA AD HA HG HB DB BC'.split()
+matrix = '346 348 12 127 678 15 458 257'.split()
+
+print(*range(1, 9))
+for i in permutations('ABCDEFGH'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x,y in graph):
+       print(*i)
+
