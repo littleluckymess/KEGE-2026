d3f675534244b4349b5a0e7fcca06275d1bd7478
User <mila.andreeva2020@mail.ru>
2026-01-05 18:09:25 +0200

    задание 3

---
+++ task_2/21401.py
@@ -0,0 +1,11 @@
+from itertools import product, permutations
+def f (x,y,w,z):
+    return x and (z <= w) and not y
+for i in product((0,1), repeat=7):
+    table = [(i[0], i[1], 1, i[2]),
+             (i[3], 1, 0, i[4]),
+             (1, 0, i[5], i[6])]
+    if len(set(table)) == len(table):
+        for p in permutations('xywz'):
+            if [f(**dict(zip(p,t))) for t in table] == [1, 1, 1]:
+                print(*p,sep='')
