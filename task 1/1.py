9eb6a3dcec96dce32408522333ca935d8a482225
sunny_milly <mila.andreeva2020@mail.ru>
2026-01-06 19:47:47 +0200

    задание 1

---
+++ task 1/1.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'AG GF FE ED DA AB GB BC CD'.split()
+matrix = '26 147 456 236 37 134 25'.split()
+
+print(*range(1,8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x,y in graph):
+        print(*i)
+
