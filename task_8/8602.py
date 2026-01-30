860c082650f2469963cf091a48a2c6859f7e0b53
sunny_milly <mila.andreeva2020@mail.ru>
2026-01-30 19:51:21 +0200

    задание 8

---
+++ task_8/8602.py
@@ -0,0 +1,12 @@
+from itertools import product
+alph = sorted('АЕКНС')
+for pos, val in enumerate(product(alph, repeat=6),1):
+    val = ''.join(val)
+    if val == 'СЕНЕКА':
+        print(pos)
+
+
+
+
+
+
