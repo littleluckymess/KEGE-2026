caea61d0e520cfb11e5a18ee73ac2d5566e15ccb
sunny_milly <mila.andreeva2020@mail.ru>
2026-01-30 19:51:31 +0200

    задание 25

---
+++ task_25/1867.py
@@ -0,0 +1,19 @@
+def f(num):
+    d = set()
+    for i in range(2, int(num **0.5) + 1):
+        if num % i == 0:
+            d |= {i, num // i}
+
+    for i in sorted(d):
+        if i % 10 == 8 and i != 8:
+            return i
+    return 0
+
+cnt = 0
+for N in range(500_001, 10 ** 20):
+    M = f(N)
+    if M:
+        print(N, M)
+        cnt += 1
+        if cnt == 5:
+            break
