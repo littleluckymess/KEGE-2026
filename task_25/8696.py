4d77e2034ed9a2e9f31202520a7f32193df1b51b
sunny_milly <mila.andreeva2020@mail.ru>
2026-02-10 20:04:11 +0200

    задание 25

--- task_25/8696.py
+++ task_25/8696.py
@@ -7,7 +7,20 @@ def is_prime(num):
 
 def f(num):
     d = set()
-    for i in range(2, int(num ** 0.5)+1):
+    for i in range(2, int(num ** .5) + 1):
         if num % i == 0:
             d |= {i, num // i}
+    if len(d) == 0:
+        return 0
+    M = sum(d)
+    if is_prime(M % 100_000):
+        return M
+    return 0
 
+cnt = 0
+for N in range(1_273_547 + 1, 10 ** 20):
+    if M := f(N):
+        print(N, M)
+        cnt += 1
+        if cnt == 5:
+            break
