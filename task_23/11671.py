7062feb723838dc01357868f1e23d302e666455a
sunny_milly <mila.andreeva2020@mail.ru>
2026-02-24 19:34:50 +0200

    задание 23 злая задача

--- task_23/11671.py
+++ task_23/11671.py
@@ -1,4 +1,35 @@
-def f(start, end):
-    if start == end: return 1
-    return f(start + 10, end) + f(start - 5, end)
-print()
+def f(start, cnt):
+  if cnt == 15:
+      return [start]
+  return f(start + 10, cnt + 1) + f(start - 5, cnt + 1)
+
+print(len(set(f(1,0))))
+
+#########################
+
+def f(start, cnt):
+  if cnt == 15:
+      return {start}
+  return f(start + 10, cnt + 1) | f(start - 5, cnt + 1)
+
+print(len(set(f(1,0))))
+
+##########################
+
+def f(start, cnt):
+  if cnt == 15:
+      ans.add(start)
+      return
+  f(start + 10, cnt + 1)
+  f(start - 5, cnt + 1)
+
+ans = set()
+f(1,0)
+print(len(ans))
+
+############################
+
+nums = {1}
+for i in range(15):
+    nums = {x + 10 for x in nums} | {x - 5 for x in nums}
+print(len(nums))
--- task_23/18267.py
+++ task_23/18267.py
@@ -2,4 +2,25 @@ def f(start, end):
     if start == end: return 1
     if start > end: return 0
     return f(start + 2, end) + f(start + 5, end) + f(start**2, end)
+
+print(f(4, 36) - 1)
+
+##############################
+
+def f(start, end):
+    if start == end: return 1
+    if start > end: return 0
+    if start == 6:
+        return  f(start + 2, end) + f(start + 5, end)
+    return f(start + 2, end) + f(start + 5, end) + f(start**2, end)
+
 print(f(4, 36))
+
+###########################
+
+def f(start, end, last):
+    if start == end and last != 'C': return 1
+    if start > end: return 0
+    return f(start + 2, end, 'A') + f(start + 5, end,'B') + f(start**2, end, 'C')
+
+print(f(4, 36,''))
