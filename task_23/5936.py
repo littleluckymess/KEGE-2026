ecd431556b88379155718a4f9b3d453ab15c3e12
sunny_milly <mila.andreeva2020@mail.ru>
2026-02-27 19:48:20 +0200

    задание 23

---
+++ task_23/5936.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def f(start, end, cnt):
+    if start % 2 != 0: cnt += 1
+    if start == end and cnt <= 4: return 1
+    if start > end or cnt > 5: return 0
+    return f(start + 2, end, cnt) + f(start + 3, end, cnt) + f(start * 2 + 1, end, cnt)
+
+print(f(1, 625, 0))
