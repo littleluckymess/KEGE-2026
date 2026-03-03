ada6424041555afa45117d3717ccb6b5e476ec54
sunny_milly <mila.andreeva2020@mail.ru>
2026-03-03 19:47:02 +0200

    задание 15 добить

---
+++ task_15/15330.py
@@ -0,0 +1,11 @@
+# from itertools import combinations
+# def f(x):
+#     B = 24 <= x <= 90
+#     C = 47 <= x <= 115
+#     A = A1 <= x <= A2
+#     return C <= (((not A) and B) <= C)
+#
+# line_A = [24,47,90,115]
+# line_x=[25,48, 91, 116]
+#
+# for A1, A2 in combinations(line_A,2):
