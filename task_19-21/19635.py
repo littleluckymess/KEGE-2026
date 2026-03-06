9aa2cf003a1a0d06eeb467bbf610e036bd6b0c21
sunny_milly <mila.andreeva2020@mail.ru>
2026-03-06 19:25:29 +0200

    задание 19-21

--- task_19-21/19635.py
+++ task_19-21/19635.py
@@ -2,15 +2,9 @@ def f(x, y, s):
     if x + y <= 100: return s % 2 == 0
     if s == 0: return False
     h = [
-        f(x - 3, y - 3, s - 1)]
-    if x % 2 == 0:
-        h += [f(x // 2, y, s - 1)]
-    else:
-        h += [f(x // 2 - 1, y, s - 1)]
-    if y % 2 == 0:
-        h += [f(x, y // 2, s - 1)]
-    else:
-        h += [f(x, y // 2 - 1, s - 1)]
+        f(x - 3, y - 3, s - 1),
+        f(x // 2, y, s - 1),
+        f(x, y // 2, s - 1)]
     return any(h) if (s - 1) % 2 == 0 else all(h)
 
 print('19)', [x for x in range(53, 1000) if f(x, 48, 2)])
