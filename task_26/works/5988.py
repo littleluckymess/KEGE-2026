2955191b514c3afd22bb17fb11bd2a726a8526a7
User <mila.andreeva2020@mail.ru>
2026-03-24 17:36:11 +0200

    злая 26 задача на 3 цвета

--- task_26/works/5988.py
+++ task_26/works/5988.py
@@ -1,11 +1,20 @@
-with open (r'../files/26_5988.txt') as file:
+with open(r'..\files\26_5988.txt') as file:
     N = int(file.readline())
     boxes = []
     for line in file:
         size, color = line.split()
         boxes.append((int(size), color))
 
-boxes = sorted(boxes, reverse=True)
+boxes = sorted(boxes)
+
+max_boxes_by_colors = {'R': [0, -6, 10 ** 4], 'G': [0, -6, 10 ** 4], 'B': [0, -6, 10 ** 4]}
+for box in boxes:
+    copy_max_boxes_by_colors = max_boxes_by_colors.copy()
+    del copy_max_boxes_by_colors[box[1]]
+    last_box = max(copy_max_boxes_by_colors.values(), key=lambda x: (box[0] - x[1] >= 7, x[0], x[1]))
+    if box[0] - last_box[1] >= 7 and max_boxes_by_colors[box[1]][0] < last_box[0] + 1:
+        max_boxes_by_colors[box[1]] = [last_box[0] + 1, box[0], min(box[0], last_box[2])]
+
 
 
 
