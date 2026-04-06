559def96a48ac804a1a95871bbfcb1c509b5c5c7
User <mila.andreeva2020@mail.ru>
2026-04-06 19:24:51 +0200

    задание 13 домашка

---
+++ task_13/11776.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network
+
+net = ip_network('235.86.56.0/255.255.248.0')
+cnt = 0
+for ip in net:
+    ip = f'{int(ip):032b}'
+    if int(ip) % 100 == 11:
+        cnt += 1
+print(cnt)
