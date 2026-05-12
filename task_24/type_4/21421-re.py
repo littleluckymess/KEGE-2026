65d7bd7949acccee605ba5e3600fea978453e0d5
sunny_milly <mila.andreeva2020@mail.ru>
2026-05-12 19:37:51 +0200

    задание 24

---
+++ task_24/type_4/21421-re.py
@@ -0,0 +1,9 @@
+from re import finditer
+with open (r'../files/24_21421.txt') as file:
+    data = file.readline()
+
+pattern = '[1-9AB][0-9AB]*[02468A]'
+
+matches = [match.group() for match in finditer(pattern, data)]
+
+print(len(max(matches, key=len)))
