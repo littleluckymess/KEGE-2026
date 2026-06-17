00f0fe36240c4f09d9ffa8fa579c4fa57b114124
sunny_milly <mila.andreeva2020@mail.ru>
2026-06-17 17:56:09 +0200

    задание 13

---
+++ task_13/17607.py
@@ -0,0 +1,11 @@
+from ipaddress import ip_network
+
+net = ip_network('115.198.0.0/255.254.0.0', False)
+
+cnt = 0
+
+for ip in net:
+    ip = f'{int(ip):032b}'
+    if ip.count('1') % 5 == 0:
+        cnt += 1
+print(cnt)
